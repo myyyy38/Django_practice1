73a795aad695088a3868b5a8031cb1e71284ac1e
yasutaka miyagawa <mygw.hgrs@gmail.com>
2022-03-12 22:22:57 +0900

    delete project

--- project/diary/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Day
-
-# Register your models here.
-
-
-admin.site.register(Day)
--- project/diary/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Day
-
-# forms htmlのformタグみたいな？
-class DayCreateForm(forms.ModelForm):
-    class Meta:
-        model = Day
-        fields = '__all__' #('title','text')などで特定フィールド指定
